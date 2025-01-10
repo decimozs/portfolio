e7990273212e3c6fab530915e7206ea52aa22bdd
decimozs <marlonadiguemartin548@gmail.com>
2025-01-10 21:51:19 +0800

    fix: improve seo

---
+++ src/lib/seo.ts
@@ -0,0 +1,32 @@
+export const seo = {
+  metadataBase: new URL("https://marlonmartin.com"),
+  title: {
+    default: "Marlon Martin — Software Developer",
+    template: "Marlon Martin — Software Developer",
+  },
+  description: "Software Developer based in the Philippines.",
+  openGraph: {
+    title: "Marlon Martin",
+    description: "Software Developer based in the Philippines.",
+    url: "https://marlonmartin.com",
+    siteName: "Marlon Martin",
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    } as const,
+  },
+  twitter: {
+    title: "Marlon Martin",
+    card: "summary_large_image",
+    description: "Software Developer based in the Philippines",
+  },
+};
