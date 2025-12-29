4ebbbd1a8762650d7313a28b3e917e4a112cff84
decimozs <marlonadiguemartin548@gmail.com>
2025-12-29 23:06:38 +0800

    chore: refactor nav and main layout

---
+++ src/components/main-layout.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+
+  return (
+    <main
+      className={`p-6 text-2xl flex flex-col ${pathname === "/" && "justify-center"} lg:items-center md:h-screen lg:w-[760px] lg:mx-auto`}
+    >
+      {children}
+    </main>
+  );
+}
