cb07627d16e15132a970d58ea0bb7dc7b508c182
decimozs <marlonadiguemartin548@gmail.com>
2025-12-30 21:14:56 +0800

    feat: change font to overused-grotesk and update header to set cursor to default

--- src/components/main-layout.tsx
+++ src/components/main-layout.tsx
@@ -11,7 +11,7 @@ export default function MainLayout({
 
   return (
     <main
-      className={`p-6 text-2xl flex flex-col ${pathname === "/" && "justify-center"} lg:items-center md:h-screen lg:w-[760px] lg:mx-auto`}
+      className={`p-6 text-2xl flex flex-col ${pathname === "/" && "justify-center"} lg:items-center md:h-screen lg:w-[765px] lg:mx-auto`}
     >
       {children}
     </main>
