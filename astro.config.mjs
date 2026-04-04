dc8de5e049ccfdee85514c30ad7ac98b8f5a8ea7
decimozs <marlonadiguemartin548@gmail.com>
2026-04-04 19:39:19 +0800

    chore: added new dependencies and format and lint files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { fileURLToPath } from "node:url";
+import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
